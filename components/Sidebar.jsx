21b1cade62213ee001ef2fa269306dd7149a16f6
roji-tech <rojitech9@gmail.com>
2022-08-09 10:44:07 +0100

    adding sidebar

---
+++ components/Sidebar.jsx
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+import { Avatar, Button, IconButton } from "@mui/material";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchIcon from "@mui/icons-material/MoreVert";
+
+const Sidebar = () => {
+  return (
+    <Container>
+      <Header>
+        <UserAvatar />
+
+        <IconContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconContainer>
+      </Header>
+
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search In Chats" />
+      </Search>
+
+      <SidebarButton> Start a new chat </SidebarButton>
+
+      {/* Chat List */}
+    </Container>
+  );
+};
+
+export default Sidebar;
+
+const Container = styled.div``;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+
+const SidebarButton = styled(Button)`
+  width: 100%;
+
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
+
+// const SearchInput = styled.input``
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const IconContainer = styled.div``;
